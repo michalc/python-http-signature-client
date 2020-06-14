87211dc81726d61e9130ab8cf644a858b37060ab
Michal Charemza <michal@charemza.name>
2020-06-14 07:59:44 +0100

    (tests) Better test name

--- tests.py
+++ tests.py
@@ -11,6 +11,33 @@ from http_signature_client import sign_headers
 
 class TestIntegration(unittest.TestCase):
 
+    def test_with_digest(self):
+        key_id = 'my-key'
+        private_key = load_pem_private_key(
+            TEST_PRIVATE_KEY, password=None, backend=default_backend())
+        method = 'post'
+        url = '/some-path?a=b&a=c&d=e'
+        body_sha512 = b64encode(hashlib.sha512(b'some-data').digest()).decode('ascii')
+        headers = (('digest', f'SHA512={body_sha512}'),)
+
+        with freeze_time('2012-01-14 03:21:34'):
+            signed_headers = sign_headers(key_id, private_key.sign, method, url, headers)
+
+        self.assertEqual(signed_headers, (
+            (
+                'authorization',
+                'Signature: keyId="my-key", created=1326511294, headers="(request-target) '
+                '(created) digest, '
+                'signature="hVRwKrNAhELt7cMBx+AKDRLlzKgJp1yKkJHh1HRM/JLlTpJOIWw56Hljpeq9tqXf1zqYWy'
+                '25bQ6vYlJeHivTCg=="',
+            ),
+            (
+                'digest',
+                'SHA512=4cT8Z/GQnjUIPMUwn8ujbSdDM6PEAJqUqXBSc+QfyIthia0VdVHj050dqkQSJk0TEgtnE8mdO+'
+                'TWTH306npMew==',
+            )
+        ))
+
     def test_no_headers(self):
         key_id = 'my-key'
         private_key = load_pem_private_key(
